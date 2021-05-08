b8fe2ea4b3b58047b9d859509e51c44d5c27e484
kumarvarun vyas <kumarvarunvyas55@gmail.com>
2021-05-08 20:08:48 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="grid-container">
+        <header>
+          <a href ="/" >React Shopping Cart</a>
+        </header>
+        <main>
+          Product List
+        </main>
+        <footer>
+          All Rights are reserved
+        </footer>
+    </div>
+  );
+}
+
+export default App;
